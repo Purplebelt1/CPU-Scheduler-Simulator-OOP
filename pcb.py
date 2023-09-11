d1546a2512a6eeb137b2977b78c140e8c5c971f8
V <138716457+vermilion-coding@users.noreply.github.com>
2023-09-11 08:50:53 -0500

    Update pcb.py

---
+++ pcb.py
@@ -0,0 +1,47 @@
+class PCB:
+
+    def __init__(self, pid, cpu_burst, 
+                 arrival_time, wait_time, priority=None):
+        if type(pid) != int:
+             raise ValueError("PID must be an integer") 
+        self.pid = int(pid)
+
+        if type(cpu_burst) != int:
+             raise ValueError("CPU burst must be an integer") 
+        self.cpu_burst = int(cpu_burst)
+        
+        if type(arrival_time) != int:
+             raise ValueError("Arrival time must be an integer") 
+        self.arrival_time = int(arrival_time)
+
+        if type(wait_time) != int:
+             raise ValueError("Wait time must be an integer") 
+        self.wait_time = int(wait_time)
+        
+        if priority is not None and type(priority) != int:
+             raise ValueError("Priority must be an integer") 
+        self.priority = priority
+
+    def __str__(self):
+        if self.priority is not None:
+            return f"Process ID: {self.pid}, Arrival Time: {self.arrival_time}, CPU Burst: {self.cpu_burst}, Priority: {self.priority}"
+        else:
+            return f"Process ID: {self.pid}, Arrival Time: {self.arrival_time}, CPU Burst: {self.cpu_burst}"
+
+    def setState(self, state):
+        self.state = state
+
+    def getState(self):
+        return self.state
+    
+    def getPID(self):
+        return self.pid
+    
+    def getCPU_burst(self):
+        return self.cpu_burst
+    
+    def getArrival_time(self):
+        return self.arrival_time
+    
+    def getPriority(self):
+        return self.priority
