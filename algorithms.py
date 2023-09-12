4b5143c2b0af17398c73ad35086b8ae26775c6ca
czahn24 <144357005+czahn24@users.noreply.github.com>
2023-09-12 11:30:49 -0500

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -15,7 +15,6 @@ def first_come_first_serve(ready_queue):
     return sorted_queue
 
 def shortest_job_first(ready_queue):
-    # Please remove type. Scheduler handles that. All I need is the returned queue.
         sorted_queue = sorted(ready_queue,  key=lambda pcb: pcb.getCPU_burst())
         if len(sorted_queue) == 0:
             return None
@@ -24,13 +23,6 @@ def shortest_job_first(ready_queue):
             
 
 def round_robin(ready_queue, algorithm):
-  #not sure if we want this as an option in scheduler or take a second parameter sorting by fcfs or sjf
-  # This is NOT round robin. Round robin sorts a queue in a two step process. There is Primary and secondary sort.
-  # This means that anything that is equal in value by the primary sorting parameter is again sorted by the secondary sorting parameter
-  # (For this case all we need to worry about is if there are multiple pcbs with a last_run of -1)
-  # The primary sort is by last_run for the pcb. Your secodary sort is by fcfs or sjf. This should only be for if you have multiple -1
-  # last_run pcbs
-
     primary_sort = sorted(ready_queue, key = lambda pcb: pcb.getLast_run())
     secondary_sort = []
     for i in primary_sort:
