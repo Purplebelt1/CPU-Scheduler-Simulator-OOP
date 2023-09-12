9a8c63282aee42727be6834e95b5f9769384ca50
V <138716457+vermilion-coding@users.noreply.github.com>
2023-09-12 11:23:50 -0500

    pcb

--- pcb.py
+++ pcb.py
@@ -1,51 +1,28 @@
 class PCB:
     def __init__(self, pid, cpu_burst, 
-<<<<<<< HEAD
                  arrival_time, priority=None):
-=======
-                 arrival_time, last_run, priority=None):
-        #Checks if pid is an integer
->>>>>>> 0e5f245886899bbaa39870269aa57385cce3dfe9
         if type(pid) != int:
              raise ValueError("PID must be an integer") 
         self.pid = int(pid)
 
-        #Checks if CPU burst is an integer
         if type(cpu_burst) != int:
              raise ValueError("CPU burst must be an integer") 
         self.cpu_burst = int(cpu_burst)
-<<<<<<< HEAD
+
         self.original_cpu_burst = int(cpu_burst)
         
         if type(arrival_time) != int:
              raise ValueError("Arrival time must be an integer") 
         self.arrival_time = int(arrival_time)
                      
-=======
-
-        #Checks if arrival time is an integer
-        if type(arrival_time) != int:
-             raise ValueError("Arrival time must be an integer") 
-        self.arrival_time = int(arrival_time)
-
-        #Checks if last run is an integer
-        if type(last_run) != int:
-             raise ValueError("Last run must be an integer") 
-        self.last_run = int(last_run)
-
-        #Checks if priority is used and priority is an integer
->>>>>>> 0e5f245886899bbaa39870269aa57385cce3dfe9
         if priority is not None and type(priority) != int:
              raise ValueError("Priority must be an integer") 
         self.priority = priority
 
-<<<<<<< HEAD
         self.first_run = None
+        
         self.last_run = -1
 
-=======
-    #Sets formatting for printing a PCB
->>>>>>> 0e5f245886899bbaa39870269aa57385cce3dfe9
     def __str__(self):
         if self.priority is not None:
             return f"Process ID: {self.pid}, Arrival Time: {self.arrival_time}, CPU Burst: {self.cpu_burst}, Priority: {self.priority}"
