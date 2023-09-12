4be5157740af4909757cda21ffbe22df9355f742
czahn24 <144357005+czahn24@users.noreply.github.com>
2023-09-12 10:48:34 -0500

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -28,10 +28,17 @@ def shortest_job_first(ready_queue, type = "NPR"):
 def round_robin(ready_queue, algorithm):
   #not sure if we want this as an option in scheduler or take a second parameter sorting by fcfs or sjf
     if algorithm == "fcfs":
-        first_come_first_serve(ready_queue)
+        sorted_queue = first_come_first_serve(ready_queue)
     elif algorithm == "sjf":
-        shortest_job_first(ready_queue)
+        sorted_queue = shortest_job_first(ready_queue)
     else:
         raise Exception("Algorithm type not supported")
+    time_slot = 1
+    running_pcb = None
+    while len(sorted_queue) > 0:
+        running_pcb = sorted_queue[0]
+        if(running_pcb.getCPU_burst <= time_slot):
+            running_pcb.setCPU_burst(0)
+            sorted_queue.pop(0)
     
     return None
