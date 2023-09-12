2b2f5c71defd61cb47512823cb99cf8a679d6e65
czahn24 <144357005+czahn24@users.noreply.github.com>
2023-09-11 19:58:06 -0500

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -7,12 +7,15 @@ def first_come_first_serve(ready_queue):
     else:
         return sorted_queue
 
-def shortest_job_first(ready_queue):
-    sorted_queue = sorted(ready_queue,  key=lambda pcb: pcb.getCPU_burst)
-    if len(sorted_queue) == 0:
-        return None
-    else:
-        return sorted_queue
+def shortest_job_first(ready_queue, type):
+    if(type == "NPR"):
+        sorted_queue = sorted(ready_queue,  key=lambda pcb: pcb.getCPU_burst)
+        if len(sorted_queue) == 0:
+            return None
+        else:
+            return sorted_queue
+    elif(type == "PRE"):
+        sorted_queue = sorted(ready_queue,  key=lambda pcb: pcb.getCPU_burst)
 
 def round_robin(ready_queue):
   #not sure if we want this as an option in scheduler or take a second parameter sorting by fcfs or sjf
