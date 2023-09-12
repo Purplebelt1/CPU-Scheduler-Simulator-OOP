e8dac0e46531566fdd464ebd77ff20dc82f9856e
V <138716457+vermilion-coding@users.noreply.github.com>
2023-09-12 07:54:16 -0500

    Comments

--- pcb.py
+++ pcb.py
@@ -1,26 +1,32 @@
 class PCB:
     def __init__(self, pid, cpu_burst, 
                  arrival_time, last_run, priority=None):
+        #Checks if pid is an integer
         if type(pid) != int:
              raise ValueError("PID must be an integer") 
         self.pid = int(pid)
 
+        #Checks if CPU burst is an integer
         if type(cpu_burst) != int:
              raise ValueError("CPU burst must be an integer") 
         self.cpu_burst = int(cpu_burst)
-        
+
+        #Checks if arrival time is an integer
         if type(arrival_time) != int:
              raise ValueError("Arrival time must be an integer") 
         self.arrival_time = int(arrival_time)
 
+        #Checks if last run is an integer
         if type(last_run) != int:
              raise ValueError("Last run must be an integer") 
         self.last_run = int(last_run)
-                     
+
+        #Checks if priority is used and priority is an integer
         if priority is not None and type(priority) != int:
              raise ValueError("Priority must be an integer") 
         self.priority = priority
 
+    #Sets formatting for printing a PCB
     def __str__(self):
         if self.priority is not None:
             return f"Process ID: {self.pid}, Arrival Time: {self.arrival_time}, CPU Burst: {self.cpu_burst}, Priority: {self.priority}"
